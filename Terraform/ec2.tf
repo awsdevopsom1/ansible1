9ce85bcef18fb09ad9d2655fa6020a73d4ebf1ea
om <awsdevopsom@gmail.com>
2025-02-03 20:40:46 +0530

    om

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "ec2" {
     ami = "ami-0b4f379183e5706b9"
     instance_type = "t3.medium"
-  
+    vpc_security_group_ids = ["sg-00af1bfc1cebcd80e"]
+    tags = {
+      Name = "ELK"
+    }
 }
